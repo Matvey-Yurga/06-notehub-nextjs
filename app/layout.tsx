ccb8eec635976008364571daef0f4aff82bc7ece
matvey <motaurga1@gmail.com>
2025-08-04 17:00:03 +0300

    +

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header/header";
-import Footer from "@/components/Footer/footer";
-import TanstackProvider from "@/components/TanStackProvider/TanstackProvider";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanstackProvider from "@/components/TanStackProvider/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
